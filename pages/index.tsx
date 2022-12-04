16a9c490683349c5af4c2fd10a8b97ce123ac431
raunak gurud <raunakgurud2121@gmail.com>
2022-12-04 19:58:56 +0530

    feat:add icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,11 +22,28 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <p className="px-1 py-2 m-2 bg-green-700">
+          Some value of x{/* <SmallBadge /> */}
+          {/* <MediumBadge num={2} /> */}
+          <LargeBadge num={9898} />
+        </p>
+        <FilledButton text="Enable" />
+        <OutlinedButton text="Enter" />
+
+        <FirstCard
+          title="New title"
+          link="http://localhost:3000"
+          date="03/12/2022"
+          image={'/../public/mypic.jpg'}
+          icon={'/../public/mypic.jpg'}
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta harum, earum alias sapiente minudunt inventore harum impedit provident consequatur quis!"
+        />
+
+
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
