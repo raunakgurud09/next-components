812ee5b04e6d67079dc2239f0150d7168c790c3a
raunak gurud <raunakgurud2121@gmail.com>
2022-12-04 20:30:41 +0530

    fix:add prettier.config file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
-import LargeBadge from "../components/Badges/Large";
-import MediumBadge from "../components/Badges/medium";
-import SmallBadge from "../components/Badges/Small";
-import FilledButton from "../components/Buttons/Filled";
-import OutlinedButton from "../components/Buttons/Outlined";
-import FirstCard from "../components/Cards/first";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import LargeBadge from '../components/Badges/Large'
+import MediumBadge from '../components/Badges/medium'
+import SmallBadge from '../components/Badges/Small'
+import FilledButton from '../components/Buttons/Filled'
+import OutlinedButton from '../components/Buttons/Outlined'
+import FirstCard from '../components/Cards/first'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -38,43 +38,6 @@ export default function Home() {
           icon={'/../public/mypic.jpg'}
           description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta harum, earum alias sapiente minudunt inventore harum impedit provident consequatur quis!"
         />
-
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
@@ -83,12 +46,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  );
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import colors from "tailwindcss/colors"} */
+
+
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode:"class",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
