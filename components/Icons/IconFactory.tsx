b3ff76e87fd9cea7ef127c93d0ff5672e32e0971
raunak gurud <raunakgurud2121@gmail.com>
2022-12-05 18:19:03 +0530

    fix:only needed icons

--- components/Icons/IconFactory.tsx
+++ components/Icons/IconFactory.tsx
@@ -1,54 +1,28 @@
-import { SVGProps } from "react";
-import { X } from "react-feather";
+import { SVGProps } from 'react'
 
-import {
-  NextJSLogo,
-  TypescriptLogo,
-  XdoXLogo,
-  ReactLogo,
-  FigmaLogo,
-  GoLogo,
-  TailwindCSSLogo,
-  NotigerLogo,
-  TwNFTLogo,
-  SecliLogo,
-} from ".";
-import StorliLogo from "./Storli";
+import { NextJSLogo, TypescriptLogo, ReactLogo, FigmaLogo, GoLogo, TailwindCSSLogo } from '.'
 
 interface IconFactoryProps extends SVGProps<SVGSVGElement> {
-  name: string;
+  name: string
 }
 
-const IconFactory = ({
-  name,
-  ...otherProps
-}: IconFactoryProps): JSX.Element => {
+const IconFactory = ({ name, ...otherProps }: IconFactoryProps): JSX.Element => {
   switch (name) {
-    case "nextjs":
-      return <NextJSLogo {...otherProps} />;
-    case "typescript":
-      return <TypescriptLogo {...otherProps} />;
-    case "xdox":
-      return <XdoXLogo {...otherProps} />;
-    case "figma":
-      return <FigmaLogo {...otherProps} />;
-    case "tailwindcss":
-      return <TailwindCSSLogo {...otherProps} />;
-    case "go":
-      return <GoLogo {...otherProps} />;
-    case "react":
-      return <ReactLogo {...otherProps} />;
-    case "notiger":
-      return <NotigerLogo {...otherProps} />;
-    case "twnft":
-      return <TwNFTLogo {...otherProps} />;
-    case "storli":
-      return <StorliLogo {...otherProps} />;
-    case "secli":
-      return <SecliLogo {...otherProps} />;
+    case 'nextjs':
+      return <NextJSLogo {...otherProps} />
+    case 'typescript':
+      return <TypescriptLogo {...otherProps} />
+    case 'figma':
+      return <FigmaLogo {...otherProps} />
+    case 'tailwindcss':
+      return <TailwindCSSLogo {...otherProps} />
+    case 'go':
+      return <GoLogo {...otherProps} />
+    case 'react':
+      return <ReactLogo {...otherProps} />
     default:
-      return <X />;
+      return <FigmaLogo />
   }
-};
+}
 
-export default IconFactory;
+export default IconFactory
