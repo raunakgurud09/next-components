b0f31b6977a302511b3476161c775a1c33786806
raunak gurud <raunakgurud2121@gmail.com>
2022-12-05 15:12:17 +0530

    feat:add reponsive nav

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Signature from './Signature'
+import SmallNav from './SmallNav'
 import ThemeSwitch from './ThemeSwitch'
 
 export interface navList {
@@ -9,7 +10,7 @@ export interface navList {
 
 export interface navList extends Array<navList> {}
 
-const navLists = [
+export const navLists = [
   {
     name: 'Blog',
     href: '/blog',
@@ -26,10 +27,11 @@ const navLists = [
 
 const Navigation = () => {
   return (
-    <nav className="flex px-6 py-4 item-center backdrop-blur font-mono sticky top-0 z-50">
+    <nav className="flex px-6 py-4 item-center select-none backdrop-blur font-mono sticky top-0 z-50">
       <Signature />
       <div className="flex-1"></div>
-      <div className="flex items-center">
+
+      <div className="hidden md:flex items-center">
         <ul className="flex items-center space-x-2">
           {navLists.map(({ name, href }: any) => (
             <div className=" px-4 py-1 rounded-md bg-slate-500/10 opacity-70 hover:opacity-100  ">
@@ -38,11 +40,14 @@ const Navigation = () => {
               </Link>
             </div>
           ))}
+          <span className="w-px h-6 mx-6 bg-900"></span>
         </ul>
-        <span className="mx-4">|</span>
-        <div className="flex align-middle justify-center bg-slate-300/10 rounded-full">
-          <ThemeSwitch />
-        </div>
+        {/* <ThemeSwitch /> */}
+      </div>
+
+      <div className="flex items-center space-x-6 text-300 md:space-x">
+        <ThemeSwitch />
+        <SmallNav />
       </div>
     </nav>
   )
