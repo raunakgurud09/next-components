215c25ded3197b6c35bf7c2906aa5eff51c57e33
raunak gurud <raunakgurud2121@gmail.com>
2022-12-05 18:21:41 +0530

    fix:update imports

---
+++ data/socials.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from 'react'
+import { GitHubLogo, HashnodeLogo, TwitterLogo, MailLogo, LinkedinLogo, FigmaLogo } from '../components/Icons'
+
+interface Social {
+  id: string
+  name: string
+  url: string
+  icon: ReactNode
+}
+
+const socials: Social[] = [
+  {
+    id: 'twitter',
+    name: 'Twitter',
+    url: 'https://twitter.com/RaunakGurud',
+    icon: <TwitterLogo />,
+  },
+  {
+    id: 'github',
+    name: 'Github',
+    url: 'https://github.com/raunakgurud09',
+    icon: <GitHubLogo />,
+  },
+  {
+    id: 'hashnode',
+    name: 'Hashnode',
+    url: 'https://raunakgurud.hashnode.dev',
+    icon: <HashnodeLogo />,
+  },
+  {
+    id: 'linkedin',
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/raunak-gurud-3b8261205/',
+    icon: <LinkedinLogo color='#fff' />,
+  },
+]
+
+export default socials
+
+// {
+//   id: 'mail',
+//   name: 'Mail',
+//   url: 'raunakgurud2121@gmail.com',
+//   icon: <MailLogo />,
+// },
