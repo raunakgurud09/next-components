218ad3482c35dd8a0c3bf7b8eacf5c3abf231129
raunak gurud <raunakgurud2121@gmail.com>
2022-12-09 22:32:43 +0530

    fix: add nav and footer to app

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { Avatar } from './Cards/first'
 import Signature from './Signature'
 import SmallNav from './SmallNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -25,6 +26,8 @@ export const navLists = [
   },
 ]
 
+const icon = '/../public/mypic.jpg'
+
 const Navigation = () => {
   return (
     <nav className="flex px-6 py-4 item-center select-none backdrop-blur font-mono sticky top-0 z-50">
@@ -34,19 +37,19 @@ const Navigation = () => {
       <div className="hidden md:flex items-center">
         <ul className="flex items-center space-x-2">
           {navLists.map(({ name, href }: any) => (
-            <div className=" px-4 py-1 rounded-md bg-slate-500/10 opacity-70 hover:opacity-100  ">
-              <Link className="text-medium font-bold" key={name} href={href}>
+            <div className=" px-4 py-1 rounded-md hover:bg-slate-500/10 opacity-70 hover:opacity-100  ">
+              <Link className="text-medium font-light" key={name} href={href}>
                 {name}
               </Link>
             </div>
           ))}
           <span className="w-px h-6 mx-6 bg-900"></span>
         </ul>
-        {/* <ThemeSwitch /> */}
       </div>
 
       <div className="flex items-center space-x-6 text-300 md:space-x">
         <ThemeSwitch />
+        <Avatar image={icon}/>
         <SmallNav />
       </div>
     </nav>
