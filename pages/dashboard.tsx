d637467eccbb6033dc40611f45a860efaf6f4bd8
raunak gurud <raunakgurud2121@gmail.com>
2022-12-22 23:45:41 +0530

    fix: protect dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,24 +2,30 @@ import { Head } from 'next/document'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-import { getSession, useSession } from 'next-auth/react'
-
-export default function Dashboard() {
-  const { data: session } = useSession()
-
+// import { getSession, useSession } from 'next-auth/react'
+import { useUser } from 'hooks/user/useUser'
+import withAuth from '@/components/withAuth'
+const Dashboard = () => {
+  // const { data: session } = useSession()
+  const { data: currentUser } = useUser()
   return (
     <>
       <div>
         <h2>Dashboard</h2>
-        {session ? User({ session }) : Guest()}
+        {currentUser ? User({ currentUser }) : Guest()}
       </div>
     </>
   )
 }
 
-function User({ session }) {
+function User({ currentUser }) {
   return (
     <>
+      user
+      {JSON.stringify(currentUser.name,null,2)}<br />
+      {JSON.stringify(currentUser.email,null,2)}<br />
+      {JSON.stringify(currentUser.image,null,2)}<br />
+      {JSON.stringify(currentUser.password,null,2)}<br />
       {/* <div>{session.user.name}</div> */}
       {/* <div>{JSON.stringify(session.user)}</div> */}
       {/* <div>{session.user.email}</div> */}
@@ -34,17 +40,19 @@ function Guest() {
   )
 }
 
-export async function getServerSideProps({ req }) {
-  const session = await getSession({ req })
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    }
-  }
-  return {
-    props: { session },
-  }
-}
+export default withAuth(Dashboard)
+
+// export async function getServerSideProps({ req }) {
+//   const { data: currentUser } = useUser()
+//   if (!currentUser) {
+//     return {
+//       redirect: {
+//         destination: '/login',
+//         permanent: false,
+//       },
+//     }
+//   }
+//   return {
+//     props: { currentUser },
+//   }
+// }
