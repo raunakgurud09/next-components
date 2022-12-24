90b5fe2afeec21c1167b0f9cdb0a1037a68465fa
raunak gurud <raunakgurud2121@gmail.com>
2022-12-24 20:30:57 +0530

    fix: badge with increment of count

--- components/Badges/Small.tsx
+++ components/Badges/Small.tsx
@@ -1,7 +1,27 @@
-export default function SmallBadge() {
+import { useEffect, useState } from 'react'
+
+export default function SmallBadge({ children, number = 2 }) {
+  const [count, setCount] = useState<number>(number)
+
+  // useEffect(() => {}, [count])
+
+  const incrementCount = () => {
+    if (count >= 9) {
+      return setCount(0)
+    }
+    setCount(count + 1)
+    // setCount((count) => count++)
+    console.log(count)
+  }
+
   return (
-    <>
-      <div className="w-1.5 h-1.5 rounded-full bg-red-800 flex-grow-0 float-right dark:bg-pink-400 "></div>
-    </>
-  );
+    <div className="max-w-fit relative justify-center items-center">
+      <div className="flex-shrink">{children}</div>
+      <div className="rounded-full w-5 h-5 bg-red-600 absolute top-[-10%] right-[0] flex justify-center items-center">
+        <div className="text-xs" onClick={incrementCount}>
+          {count}
+        </div>
+      </div>
+    </div>
+  )
 }
